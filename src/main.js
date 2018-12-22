aaadd60928099a79aa31585289faae987e041624
Maximo <maxmonteiro86@gmail.com>
2018-12-22 19:58:03 -0300

    vue-router imported

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+// importing vue router
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// registering vue router to make it enabled
+Vue.use(VueRouter)
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
