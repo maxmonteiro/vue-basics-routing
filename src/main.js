7aa516a03c0121177ae4129494f345578e23cae0
Maximo <maxmonteiro86@gmail.com>
2018-12-22 20:07:35 -0300

    Setting routes

--- src/main.js
+++ src/main.js
@@ -2,11 +2,21 @@ import Vue from 'vue'
 // importing vue router
 import VueRouter from 'vue-router'
 import App from './App.vue'
+// importing components to use in routes
+import Home from './components/Home.vue'
+import Users from './components/Users.vue'
 
 Vue.config.productionTip = false
 
 // registering vue router to make it enabled
 Vue.use(VueRouter)
+
+// list of routes
+const routes = [
+    { path: '/', component: Home },
+    { path: '/users', component: Users }
+]
+
 
 new Vue({
     render: h => h(App),
