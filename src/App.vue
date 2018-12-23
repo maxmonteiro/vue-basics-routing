2f476c09e6bc3ccc7e87ad3a1381c60647d28d4a
Maximo <maxmonteiro86@gmail.com>
2018-12-22 21:19:24 -0300

    Created links navigation with router-link

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
   <div id="app">
     <h1>Let's go somewhere!</h1>
     <hr>
+    <nav class="nav">
+      <router-link to="/" class="nav-link">Home</router-link>
+      <router-link to="/users" class="nav-link">Users</router-link>
+    </nav>
+    <hr>
     <router-view></router-view>
   </div>
 </template>
